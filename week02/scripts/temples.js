8a61af5f83c4342177a2a6218673c9bc6ef2cf29
csprouse91 <csprouse2010@live.com>
2025-04-02 21:48:45 -0600

    Picture Album Update

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,2 +1,10 @@
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+const hamburgerButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamburgerButton.addEventListener('click', () => {
+    hamburgerButton.classList.toggle('open');
+    navigation.classList.toggle('open');
+});
