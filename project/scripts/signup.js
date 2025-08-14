7f1956da9682a0be8ea35bff197cf7ee5d6f616e
csprouse91 <csprouse2010@live.com>
2025-08-14 11:18:44 -0600

    Week 6 Project Progress Push

---
+++ project/scripts/signup.js
@@ -0,0 +1,43 @@
+// Array of age groups
+const ageGroups = [
+    {
+        league: "T-Ball",
+        age: "5U"
+    },
+    {
+        league: "T-Ball",
+        age: "7U"
+    },
+    {
+        league: "Minor - Coach Pitch",
+        age: "7U"
+    },
+    {
+        league: "Minor - Machine Pitch",
+        age: "9U"
+    },
+    {
+        league: "Minor - Machine Pitch / Hybrid Kid Pitch",
+        age: "9U"
+    },
+    {
+        league: "Minor - Kid Pitch",
+        age: "9U"
+    },
+    {
+        league: "Major",
+        age: "11U"
+    },
+    {
+        league: "Major",
+        age: "12U"
+    },
+    {
+        league: "Junior",
+        age: "14U"
+    },
+    {
+        league: "Senior",
+        age: "16U"
+    }
+];
